ad9783258b7926404bf1476d84ccaafef08b95fd
YU WU <jenniferwu617@gmail.com>
2020-10-30 02:18:44 +0800

    新增filter搜尋input & sort

--- src/product/components/ProductEight.js
+++ src/product/components/ProductEight.js
@@ -6,32 +6,32 @@ function ProductEight(props) {
     <>
       <div class="container related-product">
         <div class="row justify-content-center title">
-          <h3>其他人也喜歡</h3>
+          <h3>猜你喜歡</h3>
         </div>
         <div class="row">
           <div class="col-sm-6 col-lg-3">
             <div class="related-img">
               <img src={popularImg} alt="" />
             </div>
-            <p>HAFLING-MERAN, ITALY</p>
+            <p>西班牙馬貝拉-野獸派酒吧椅</p>
           </div>
           <div class="col-sm-6 col-lg-3">
             <div class="related-img">
               <img src={popularImg} alt="" />
             </div>
-            <p>HAFLING-MERAN, ITALY</p>
+            <p>山毛櫸曲木扶手椅</p>
           </div>
           <div class="col-sm-6 col-lg-3">
             <div class="related-img">
               <img src={popularImg} alt="" />
             </div>
-            <p>HAFLING-MERAN, ITALY</p>
+            <p>Gastone Rinaldi 金屬牛皮椅</p>
           </div>
           <div class="col-sm-6 col-lg-3">
             <div class="related-img">
               <img src={popularImg} alt="" />
             </div>
-            <p>HAFLING-MERAN, ITALY</p>
+            <p>Casala 玫瑰木高背餐椅</p>
           </div>
         </div>
       </div>
