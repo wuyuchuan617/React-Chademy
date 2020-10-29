10b69026a659ef5dbeddd9165323f3b111ce18cc
Grace <yurisine7@gmail.com>
2020-10-29 21:04:08 +0800

    actions

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,5 +1,5 @@
-export const SWIPE_LEFT = 'SWIPE_LEFT'
-export const SWIPE_RIGHT = 'SWIPE_RIGHT'
+export const GET_DATA = 'GET_DATA'
+// export const SWIPE_RIGHT = 'SWIPE_RIGHT'
 
 // 新增初始化數值的動作類型
 export const INIT_ACT = 'INIT_ACT'
