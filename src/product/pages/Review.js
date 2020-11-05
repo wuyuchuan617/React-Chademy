b76609e92508ba36370b73d94f0202d52c80449c
吳宇涓 <wuyuchuan@wuyujuande-MacBook-Pro.local>
2020-11-06 02:54:34 +0800

    評論套上會員工版

--- src/product/pages/Review.js
+++ src/product/pages/Review.js
@@ -3,10 +3,17 @@
 import React, { useState, useEffect } from 'react'
 import { Rate, Upload, Button } from 'antd'
 import { UploadOutlined } from '@ant-design/icons'
+import { Layout } from 'antd'
+// ------------------以下引入Components----------------------------
+
+import Aside from '../../common_components/Aside/index'
 
 // ------------------------以下引入樣式----------------------------
 
 import '../styles/review.css'
+import '../../common_components/Aside/index.scoped.scss'
+import '../../member/MemberCenter/index.scoped.scss'
+const { Header, Footer, Sider, Content } = Layout
 
 // -----------------------以下開始Component Product-----------------
 
@@ -32,6 +39,8 @@ function Review(props) {
   const [stars, setStars] = useState(5)
 
   // ---------------以下開始fetch SQL get data function-----------------
+
+  // 拿訂單資料
 
   // 圖片預覽
   async function updateReviewToServer() {
@@ -103,175 +112,172 @@ function Review(props) {
 
   return (
     <>
-      <div className="container custom-container-width">
-        <div className=" text-center m-5">
-          <p>新增評論</p>
-          <p>您好，Amy 感謝您的購買</p>
-        </div>
-        <form
-          className="formcss1"
-          action=""
-          name="form1"
-          novalidate
-          onSubmit={handleSubmit}
-        >
-          <div className="row no-gutters">
-            <div className="col-lg-6 col-sm-12">
-              <div className="upload">
-                <div
-                  className="btn2 text-center"
-                  onClick={() => {
-                    // updateReviewToServer()
-                  }}
-                >
-                  上傳照片
-                </div>
-              </div>
-              <p className="text-center notice">＊可以點選或拖曳上傳圖片</p>
+      <Layout className="memberCenter_container">
+        <Header className="hander_container">
+          <div className="title">我的評論</div>
+        </Header>
 
-              <div className="upload_img">
-                <input
-                  type="hidden"
-                  id="photo"
-                  name="photo"
-                  value={previewPhoto.newFileName}
-                  className="form-control"
-                />
-                <img
-                  src={`http://localhost:3001/img/` + previewPhoto.newFileName}
-                  alt=""
-                  id="myimg"
-                />
-                <input
-                  type="file"
-                  id="file_input"
-                  // name="myfile"
-                  onChange={(e) => {
-                    console.log(e.target.files[0])
-                    const newPhoto = e.target.files[0]
-
-                    setPhoto(newPhoto)
-                    // await updateReviewToServer()
-                  }}
-                />
-              </div>
-              <div
-                className="btnDemo text-center mt-4"
-                onClick={() => {
-                  setOrderProductNo('CH29')
-                  setOrderProduct('204RH')
-                  setOrderNo('OVER1234')
-                  setOrderDate('2020.08.31')
-                  setReviewTitle('質感非常好！')
-                  setReviewComment(
-                    `過去幾年來，我們創作了幾個空間，秉持著NV一向自然、原始、純粹的風格，希望讓大家拋開過去非得要”裝潢”的概念，讓空間載體本身回歸乾淨簡單，再佐上我們為您搭配的歐洲經典傢俱家飾物件，由我們親自設計施作的木作，就是要讓您展現任性、自豪又絕對脫俗的居家品味。“把喜歡的美感傳達出去`
-                  )
-                }}
-              >
-                DEMO
-              </div>
+        <Layout>
+          <Sider
+            className="sider_container"
+            style={{ background: 'transparent' }}
+            light="light"
+            width={256}
+          >
+            <Aside></Aside>
+          </Sider>
+          <Content style={{ padding: '0 50px' }}>
+            <div className=" text-center my-2">
+              <p>新增評論</p>
+              <p>您好，Amy 感謝您的購買</p>
             </div>
-            <div className="col-lg-6 col-sm-12">
-              {/* <form className="formcss" action=""> */}
-              <div className="d-flex justify-content-between">
-                <div className="formset reviewInput">
-                  <label for="productname">商品名稱</label>
+            <form
+              className="w_formcss1"
+              action=""
+              name="form1"
+              novalidate
+              onSubmit={handleSubmit}
+            >
+              <div className="d-flex justify-content-end">
+                <div className="col-lg-5 col-sm-12">
+                  <div className="w_upload">
+                    <div
+                      className="btn2 text-center"
+                      onClick={() => {
+                        // updateReviewToServer()
+                      }}
+                    >
+                      上傳照片
+                    </div>
+                  </div>
+                  <p className="text-center notice">＊可以點選或拖曳上傳圖片</p>
+
+                  <div className="upload_img">
+                    <input
+                      type="hidden"
+                      id="photo"
+                      name="photo"
+                      value={previewPhoto.newFileName}
+                      className="form-control"
+                    />
+                    <img
+                      src={
+                        `http://localhost:3001/img/` + previewPhoto.newFileName
+                      }
+                      alt=""
+                      id="myimg"
+                    />
+                    <input
+                      type="file"
+                      id="file_input"
+                      // name="myfile"
+                      onChange={(e) => {
+                        console.log(e.target.files[0])
+                        const newPhoto = e.target.files[0]
+
+                        setPhoto(newPhoto)
+                        // await updateReviewToServer()
+                      }}
+                    />
+                  </div>
+                  <div
+                    className="btnDemo text-center mt-4"
+                    onClick={() => {
+                      setOrderProductNo('CH29')
+                      setOrderProduct('204RH')
+                      setOrderNo('OVER1234')
+                      setOrderDate('2020.08.31')
+                      setReviewTitle('質感非常好！')
+                      setReviewComment(
+                        `過去幾年來，我們創作了幾個空間，秉持著NV一向自然、原始、純粹的風格，希望讓大家拋開過去非得要”裝潢”的概念，讓空間載體本身回歸乾淨簡單，再佐上我們為您搭配的歐洲經典傢俱家飾物件，由我們親自設計施作的木作，就是要讓您展現任性、自豪又絕對脫俗的居家品味。“把喜歡的美感傳達出去`
+                      )
+                    }}
+                  >
+                    DEMO
+                  </div>
+                </div>
+                <div className="col-lg-5 col-sm-12">
+                  {/* <form className="formcss" action=""> */}
+                  <div className="d-flex justify-content-between">
+                    <div className="formset reviewInput">
+                      <label for="productname">商品名稱</label>
+                      <input
+                        type="text"
+                        className="formstyle formwidthw1"
+                        value={orderProduct}
+                        id="productname"
+                        name="buy_product"
+                      />
+                    </div>
+                    <div className="formset reviewInput">
+                      <label for="productname">訂單編號</label>
+                      <input
+                        type="text"
+                        className="formstyle formwidthw1"
+                        value={orderProductNo}
+                        name="order_no"
+                        id="productname"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="reviewStars d-flex">
+                    <Rate
+                      style={{ color: '#C77334' }}
+                      defaultValue={5}
+                      onChange={(e) => {
+                        setStars(e)
+                      }}
+                    />
+                  </div>
+
                   <input
-                    type="text"
-                    className="formstyle formwidthw"
-                    value={orderProduct}
-                    id="productname"
-                    name="buy_product"
+                    type="hidden"
+                    name="stars"
+                    value={stars}
+                    className="form-control"
+                  />
+
+                  <div className="formset">
+                    <label for="price">評論標題</label>
+                    <input
+                      type="text"
+                      className="formstyle formwidthw"
+                      value={reviewTitle}
+                      id="price"
+                      name="review_title"
+                    />
+                  </div>
+
+                  <input
+                    type="hidden"
+                    name="buy_member_id"
+                    value="AMY"
+                    className="form-control"
+                  />
+
+                  <div className="formset textareaRewiew">
+                    <label for="description">商品評論</label>
+                    <textarea
+                      rows="10"
+                      style={{ resize: 'none' }}
+                      cols="44"
+                      className="formstyle"
+                      value={reviewComment}
+                      name="review_comment"
+                    ></textarea>
+                  </div>
+                  <input
+                    type="submit"
+                    value="新增評論"
+                    className="btnReview text-center mt-4"
                   />
                 </div>
-                <div className="formset reviewInput">
-                  <label for="product_no">購買日期</label>
-                  <input
-                    type="text"
-                    className="formstyle formwidthw"
-                    value={orderDate}
-                    id="product_no"
-                  />
-                </div>
               </div>
-
-              <div className="d-flex justify-content-between">
-                <div className="formset reviewInput">
-                  <label for="productname">商品編號</label>
-                  <input
-                    type="text"
-                    className="formstyle formwidthw"
-                    value={orderProductNo}
-                    id="productname"
-                  />
-                </div>
-                <div className="formset reviewInput">
-                  <label for="product_no">訂單編號</label>
-                  <input
-                    type="text"
-                    className="formstyle formwidthw"
-                    value={orderNo}
-                    id="product_no"
-                    name="order_no"
-                  />
-                </div>
-              </div>
-
-              <div className="reviewStars d-flex">
-                <Rate
-                  style={{ color: '#C77334' }}
-                  defaultValue={5}
-                  onChange={(e) => {
-                    setStars(e)
-                  }}
-                />
-              </div>
-
-              <input
-                type="hidden"
-                name="stars"
-                value={stars}
-                className="form-control"
-              />
-
-              <div className="formset">
-                <label for="price">評論標題</label>
-                <input
-                  type="text"
-                  className="formstyle formwidthw"
-                  value={reviewTitle}
-                  id="price"
-                  name="review_title"
-                />
-              </div>
-
-              <input
-                type="hidden"
-                name="buy_member_id"
-                value="AMY"
-                className="form-control"
-              />
-
-              <div className="formset textareaRewiew">
-                <label for="description">商品評論</label>
-                <textarea
-                  rows="10"
-                  style={{ resize: 'none' }}
-                  cols="65"
-                  className="formstyle"
-                  value={reviewComment}
-                  name="review_comment"
-                ></textarea>
-              </div>
-              <input
-                type="submit"
-                value="新增評論"
-                className="btnReview text-center mt-4"
-              />
-            </div>
-          </div>
-        </form>
-      </div>
+            </form>
+          </Content>
+        </Layout>
+      </Layout>
     </>
   )
 }
