a9fd7af8f72daac46d13edab4997f91215ed3dd0
JayLee <jieyi840713@gmail.com>
2020-11-11 12:27:50 +0800

    updated

--- src/cart/components/AllOrder.js
+++ src/cart/components/AllOrder.js
@@ -239,6 +239,7 @@ function AllOrder(props) {
                 }}
                 onClick={() => {
                   props.history.push(link)
+                  setVisible(false)
                 }}
               >
                 <BsPen />
