1fce9c267c266a69b3c685d57195039e26b81abc
cy <lmjyy5v8331@gmail.com>
2020-11-12 04:23:15 +0800

    Update - 地址, eslint config

--- src/member/Address/index.js
+++ src/member/Address/index.js
@@ -1,129 +1,220 @@
 // import React, { useState, useEffect } from 'react'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './index.scoped.scss'
 
-import { Button, Modal } from 'react-bootstrap'
-import { countries, townships } from '../../cart/components/Data'
+import { countries, cityData } from '../../cart/components/Data'
+import request from '../../utils/request'
+import { transAddress } from '../../utils'
 
+import { message, Modal, Input, Select } from 'antd'
+import { BsTrash } from 'react-icons/bs'
 import { AiOutlinePlus } from 'react-icons/ai'
+import { ExclamationCircleOutlined } from '@ant-design/icons'
+
+const { Option } = Select
+const { confirm } = Modal
 
 function Address() {
-  const [city, setCity] = useState(-1)
-  const [area, setArea] = useState(-1)
-  const [modalShow, setModalShow] = useState(false)
-  const [smShow, setSmShow] = useState(false)
-  //   const [modalShow, setModalShow] = useState(false)
+  const [allAddress, allSetAddress] = useState('')
 
-  //   const handleOpenModal = () => {
-  //     console.log('Click')
-  //     setModalShow(true)
-  //   }
+  const [defaultCity, setDefaultCity] = useState('')
+  const [cities, setCities] = useState({
+    city: '',
+    cities: cityData[countries[0]],
+    secondCity: cityData[countries[0]][0],
+  })
 
-  //   const handleSubmit = (event) => {
-  //     event.preventDefault()
-  //     setModalShow(false)
-  //   }
+  const [otherAddress, setOtherAddress] = useState('')
 
-  // useEffect(() => {
-  //   console.log('city: ', city)
-  // }, [city])
+  const [visible, setVisible] = useState(false)
+
+  const handleProvinceChange = (value) => {
+    setCities({
+      city: value,
+      cities: cityData[value],
+      secondCity: cityData[value][0],
+    })
+  }
+
+  const onSecondCityChange = (value) => {
+    setCities({ ...cities, secondCity: value })
+  }
+
+  const setData = (address) => {
+    // 將字串的地址轉換
+    const { cityId, areaId, otherAddress } = transAddress(address)
+
+    // 設定畫面上的全部地址
+    allSetAddress(address)
+
+    // 設定 Select 組件的預設值
+    setDefaultCity(countries[cityId])
+
+    // 設定 Options 組件的值
+    setCities({
+      city: countries[cityId],
+      cities: cityData[countries[cityId]],
+      secondCity: cityData[countries[cityId]][areaId],
+    })
+
+    // 設定 Input 組件的值
+    setOtherAddress(otherAddress)
+  }
+
+  function showConfirm() {
+    confirm({
+      title: '確定要刪除地址嗎?',
+      icon: <ExclamationCircleOutlined />,
+      onOk() {
+        deleteAddress()
+      },
+      onCancel() {
+        console.log('Cancel')
+      },
+    })
+  }
+
+  const getAddress = async () => {
+    const response = await request({
+      url: `/members/getAddress`,
+      method: 'POST',
+      data: {},
+    })
+
+    const { success, data } = response
+
+    if (success) {
+      // 設定地址
+      setData(data.address)
+    }
+  }
+
+  const deleteAddress = async () => {
+    const response = await request({
+      url: `/members/deleteAddress`,
+      method: 'POST',
+      data: {},
+    })
+
+    const { success, msg } = response
+
+    if (!success) {
+      message.error(msg)
+    } else {
+      message.success(msg)
+      getAddress()
+    }
+  }
+
+  const setAddress = async (payload) => {
+    const response = await request({
+      url: `/members/setAddress`,
+      method: 'POST',
+      data: payload,
+    })
+
+    const { success, msg, data } = response
+
+    if (!success) {
+      message.error(msg)
+    } else {
+      message.success(msg)
+      // 成功，將回傳地址設定回去(跟 getAddress 一樣)
+      setData(data.address)
+    }
+
+    // 關掉彈窗
+    setVisible(false)
+  }
+
+  const handleOk = () => {
+    // 檢查是否正確輸入地址
+    if (!otherAddress.length) {
+      confirm({
+        title: '請輸入地址',
+        icon: <ExclamationCircleOutlined />,
+        onOk() {
+          console.log('OK')
+        },
+      })
+
+      return
+    }
+
+    const { city, secondCity } = cities
+
+    // 將地址組起來，以物件的方式傳入函式
+    setAddress({ address: `${city}${secondCity}${otherAddress}` })
+  }
+
+  // didMount 拿所有資料
+  useEffect(() => {
+    getAddress()
+  }, [])
 
   return (
     <>
       <div className="lineee justify-content-between">
-        新增地址
+        地址：{allAddress ? allAddress : '新增地址'}
         <div className="icon_con">
-          <AiOutlinePlus className="icon" onClick={() => setModalShow(true)} />
+          <AiOutlinePlus className="icon" onClick={() => setVisible(true)} />
+          {allAddress && (
+            <BsTrash
+              className="delet_btn"
+              onClick={() => showConfirm()}
+            ></BsTrash>
+          )}
         </div>
       </div>
 
       <Modal
-        // {...props}
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
+        title={allAddress ? '編輯地址' : '新增地址'}
+        visible={visible}
+        onOk={handleOk}
+        maskClosable={false}
+        onCancel={() => setVisible(false)}
       >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">新增地址</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <h4>新增地址</h4>
-
-          <select
+        <section>
+          <Select
             style={{
-              width: '290px',
+              width: 'calc(50% - 20px)',
               height: '40px',
               marginBottom: '30px',
               marginRight: '20px',
             }}
-            onChange={(e) => {
-              setCity(+e.target.value)
-              setArea(-1)
-            }}
+            size="large"
+            defaultValue={defaultCity}
+            onChange={handleProvinceChange}
           >
-            <option value={-1}>城市</option>
-            {countries.map((v, i) => (
-              <option key={i} value={i}>
-                {v}
-              </option>
+            {countries.map((province) => (
+              <Option key={province}>{province}</Option>
             ))}
-          </select>
-          <select
+          </Select>
+          <Select
             style={{
-              width: '290px',
+              width: '50%',
               height: '40px',
               marginBottom: '30px',
             }}
-            value={area}
-            onChange={(e) => {
-              setArea(+e.target.value)
-            }}
+            size="large"
+            value={cities.secondCity}
+            onChange={onSecondCityChange}
           >
-            <option value={-1}>區域</option>
-            {city > -1 &&
-              townships[city].map((v, i) => (
-                <option key={i} value={i}>
-                  {v}
-                </option>
-              ))}
-          </select>
-
-          <input
-            type="text"
+            {cities.cities.map((city) => (
+              <Option key={city}>{city}</Option>
+            ))}
+          </Select>
+          <Input
+            size="large"
             style={{
-              width: '600px',
+              width: '100%',
               height: '40px',
-              marginBottom: '30px',
             }}
+            value={otherAddress}
             placeholder="地址"
+            onChange={(e) => setOtherAddress(e.target.value)}
           />
-
-          <Button
-            variant="primary"
-            type="submit"
-            className="for_btn"
-            // onClick={async () => {
-            //   const response = await request({
-            //     url: `/members/forgetPwd`,
-            //     method: 'POST',
-            //     data: { email },
-            //   })
-
-            //   if (!response.success) {
-            //     message.error(response.msg)
-            //   } else {
-            //     message.success(response.msg)
-            //   }
-            // }}
-          >
-            發送
-          </Button>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={() => setModalShow(false)}>Close</Button>
-        </Modal.Footer>
+        </section>
       </Modal>
     </>
   )
