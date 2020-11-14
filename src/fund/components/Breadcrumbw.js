fcc6b4b9434f07e72e12f19b4b9f9708de3f46eb
Eva1009 <tctct785@gmail.com>
2020-11-14 14:39:39 +0800

    募資麵包屑

---
+++ src/fund/components/Breadcrumbw.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react'
+import { Link, withRouter } from 'react-router-dom'
+import { Breadcrumb } from 'antd'
+
+function Breadcrumbw(props) {
+  const { item } = props
+  console.log(item)
+
+  let path = ''
+  const pathname = props.location.pathname
+
+  switch (pathname) {
+    case '/product':
+      path = '經典產品'
+      break
+    case '/brand':
+      path = '品牌故事'
+      break
+    case '/secondhand_list':
+      path = '中古市集'
+      break
+    default:
+      path = ''
+  }
+
+  return (
+    // <>
+    //   <nav aria-label="breadcrumb">
+    //     <ol className="breadcrumb">
+    //       <li className="breadcrumb-item">
+    //         <Link to="/">首頁</Link>
+    //       </li>
+    //       <li className="breadcrumb-item active" aria-current="page">
+    //         {path}
+    //       </li>
+    //     </ol>
+    //   </nav>
+    // </>
+    <Breadcrumb>
+      <Breadcrumb.Item>
+        <Link to="/" style={{ fontSize: '14px' }}>
+          首頁
+        </Link>
+      </Breadcrumb.Item>
+      <Breadcrumb.Item>
+        <Link to="/fundlist" style={{ fontSize: '14px' }}>
+          募資專案
+        </Link>
+      </Breadcrumb.Item>
+    </Breadcrumb>
+  )
+}
+
+export default withRouter(Breadcrumbw)
