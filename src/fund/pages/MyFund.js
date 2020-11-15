0f1eae0e7b30721b2eff90fbdeb82bedeb2488cf
cy <lmjyy5v8331@gmail.com>
2020-11-15 16:16:00 +0800

    Update 募資

--- src/fund/pages/MyFund.js
+++ src/fund/pages/MyFund.js
@@ -1,129 +1,133 @@
 import React, { useState, useEffect } from 'react'
+
+// 引入封裝過的 axios
+import request from '../../utils/request'
+import { userInfo } from '../../utils'
+
+// import { notification, Space } from 'antd'
+
+// style 引入順序，後者會蓋前者
 import '../../secondhand/styles/myproduct.css'
-import { Rate, Upload, Layout } from 'antd'
-import { notification, Space } from 'antd'
+import '../../member/index.scoped.scss'
 import '../../secondhand/styles/secondhandForm.css'
 
-import Aside from '../../common_components/Aside/index'
-import '../../common_components/Aside/index.scoped.scss'
-import '../../member/MemberCenter/index.scoped.scss'
-
 import MyproductTd from '../components/MyFundTd'
+import NoData from '../../common_components/NoData'
 
-const { Header, Footer, Sider, Content } = Layout
+// import { Layout } from 'antd'
+// const { Content } = Layout
 
 const Myproduct = () => {
-  const [member, setMember] = useState('')
-  const [memberSid, setMemberSid] = useState('')
+  const { name, sid } = userInfo()
+
+  // const [member, setMember] = useState('')
   const [myproduct, setMyprdouct] = useState([])
+  // const [memberSid, setMemberSid] = useState('')
 
-  //redux
-  function getCartFromLocalStorage() {
-    const newMember = JSON.parse(localStorage.getItem('reduxState')).user.users
-      .name
+  // //redux
+  // function getCartFromLocalStorage() {
+  //   const newMember = JSON.parse(localStorage.getItem('reduxState')).user.users
+  //     .name
 
-    console.log('newMember', newMember)
-    console.log(typeof newMember)
-    setMember(newMember)
-  }
+  //   console.log('newMember', newMember)
+  //   console.log(typeof newMember)
+  //   setMember(newMember)
+  // }
 
-  //member sid
-  function getCartFromLocalStorage2() {
-    const newMember = JSON.parse(localStorage.getItem('reduxState')).user.users
-      .sid
+  // //member sid
+  // function getCartFromLocalStorage2() {
+  //   const newMember = JSON.parse(localStorage.getItem('reduxState')).user.users
+  //     .sid
 
-    console.log('newMember', newMember)
-    console.log(typeof newMember)
-    setMemberSid(newMember)
-    console.log('memberSid', memberSid)
-  }
+  //   console.log('newMember', newMember)
+  //   console.log(typeof newMember)
+  //   setMemberSid(newMember)
+  //   console.log('memberSid', memberSid)
+  // }
 
   /**
    * 撈指定會員資料
    */
-  async function getItemFromSQL3() {
-    const url = 'http://localhost:3001/man_fund/fundlistnode/' + memberSid
+  // async function getItemFromSQL3() {
+  //   const url = 'http://localhost:3001/man_fund/fundlistnode/' + memberSid
 
-    const request = new Request(url, {
-      method: 'GET',
+  //   const request = new Request(url, {
+  //     method: 'GET',
 
-      headers: new Headers({
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      }),
-    })
-    // const response = await fetch(request).then((v) => console.log(v))
-    const response = await fetch(request)
-    const data = await response.json()
+  //     headers: new Headers({
+  //       Accept: 'application/json',
+  //       'Content-Type': 'application/json',
+  //     }),
+  //   })
+  //   // const response = await fetch(request).then((v) => console.log(v))
+  //   const response = await fetch(request)
+  //   const data = await response.json()
 
-    console.log('response', response) // [object Response]
-    console.log('data', data) // [object Object]
+  //   console.log('response', response) // [object Response]
+  //   console.log('data', data) // [object Object]
 
-    setMyprdouct(data)
-  }
+  //   setMyprdouct(data)
+  // }
+
+  // useEffect(() => {
+  //   getCartFromLocalStorage()
+  //   getCartFromLocalStorage2()
+  // }, [])
+
+  // useEffect(() => {
+  //   getItemFromSQL3()
+  // }, [memberSid])
 
   useEffect(() => {
-    getCartFromLocalStorage()
-    getCartFromLocalStorage2()
-  }, [])
+    async function fetchData() {
+      // 封裝的 axios，請求會帶上 token，因此前端不用多帶 user_sid
+      // 後端直接可以透過 req.session.sid 知道誰登入
+      const response = await request({
+        url: `/man_fund/fundlistnode`,
+        method: 'POST',
+      })
+      const { success, data } = response
 
-  useEffect(() => {
-    getItemFromSQL3()
-  }, [memberSid])
+      console.log('success, data ', success, data)
+      if (success) {
+        setMyprdouct(data)
+      }
+    }
 
-  useEffect(() => {
-    getItemFromSQL3()
-  }, [myproduct])
+    fetchData()
+  }, [name, sid])
 
   return (
     <>
-      <Layout className="memberCenter_container">
-        {/* <Header className="hander_container">
-          <div className="title">我的募資</div>
-        </Header> */}
+      <div className="i_title text-center">
+        <h5>Hello, {name}</h5>
+      </div>
 
-        <Layout>
-          {/* <Sider
-            className="sider_container"
-            style={{ background: 'transparent' }}
-            light="light"
-            width={256}
-          >
-            <Aside></Aside>
-          </Sider> */}
-          <Content style={{ padding: '0 px' }}>
-            <div className="i_title text-center">
-              <h5>Hello, {member}</h5>
-            </div>
-
-            <div className="container mycontainer">
-              <table className="table table-hover">
-                <thead>
-                  <tr>
-                    <th scope="col">募資編號</th>
-                    <th scope="col">產品圖片</th>
-                    <th scope="col">專案名稱</th>
-                    <th scope="col">最低價格</th>
-                    <th scope="col">刪除</th>
-                    <th scope="col">編輯</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {myproduct.map((item, index) => {
-                    return (
-                      <MyproductTd
-                        key={index}
-                        item={item}
-                        myproduct={myproduct}
-                      />
-                    )
-                  })}
-                </tbody>
-              </table>
-            </div>
-          </Content>
-        </Layout>
-      </Layout>
+      <div className="container mycontainer">
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">募資編號</th>
+              <th scope="col">產品圖片</th>
+              <th scope="col">專案名稱</th>
+              <th scope="col">最低價格</th>
+              <th scope="col">刪除</th>
+              <th scope="col">編輯</th>
+            </tr>
+          </thead>
+          <tbody>
+            {myproduct.length > 0 ? (
+              myproduct.map((item, index) => {
+                return (
+                  <MyproductTd key={index} item={item} myproduct={myproduct} />
+                )
+              })
+            ) : (
+              <NoData tips="沒有募資清單"></NoData>
+            )}
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
