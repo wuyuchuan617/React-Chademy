84df074ac04f6d01f5c389adc858a611998dc47e
JayLee <jieyi840713@gmail.com>
2020-11-17 20:08:36 +0800

    updated

--- src/cart/components/OrderToFund.js
+++ src/cart/components/OrderToFund.js
@@ -8,6 +8,57 @@ function OrderToFund(props) {
     <>
       <div className="j_myorderbox" style={{ marginBottom: '15px' }}>
         <div className="j_wrapspace">
+          <div style={{ display: 'flex' }}>
+            <img
+              style={{
+                width: '450px',
+                height: '293px',
+                objectFit: 'cover',
+              }}
+              src={
+                'http://localhost:3001/img/0cdc95af85269592688c630e45502d3c.png'
+              }
+              alt=""
+            />
+            <div style={{ marginLeft: '20px', width: '350px' }}>
+              <h5 style={{ marginTop: '10px' }}>訂單編號:PO1604924958983</h5>
+              <h6 style={{ marginTop: '50px' }}>訂購日期: 2020-11-15</h6>
+              <h6 style={{ marginTop: '30px' }}>訂單狀況:已完成</h6>
+              <h6 style={{ marginTop: '30px' }}>訂單金額:$20,000</h6>
+            </div>
+            <div className="j_box1">
+              <div style={{ marginLeft: '90px' }}>
+                {/* <Dropdown
+                  overlay={menu}
+                  trigger={['click']}
+                  placement={'bottomRight'}
+                  onClick={() => {
+                    setMyPO_NO(item.PO_NO)
+                    setMyDate(item.order_date)
+                  }}
+                >
+                  <a
+                    className="ant-dropdown-link"
+                    onClick={(e) => e.preventDefault()}
+                  >
+                    <h4>
+                      <GrMore />
+                    </h4>
+                  </a>
+                </Dropdown> */}
+              </div>
+              <Link to="/fundform">
+                <h6 style={{ marginTop: '230px', cursor: 'pointer' }}>
+                  我要募資
+                  <FaSketch />
+                </h6>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="j_myorderbox" style={{ marginBottom: '15px' }}>
+        <div className="j_wrapspace">
           <h5>訂單編號:PO1604924958983</h5>
         </div>
         <h6 style={{ marginTop: '30px' }}>訂購日期: 2020-11-15</h6>
@@ -32,7 +83,7 @@ function OrderToFund(props) {
             </h6>
           </Link>
         </div>
-      </div>
+      </div> */}
     </>
   )
 }
